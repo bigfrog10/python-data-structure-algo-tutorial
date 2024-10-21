6d2b7df159b320fdf14cc73bd9913b11fb86d93b
rcui <cuirick@gmail.com>
2024-10-21 07:56:14 -0400

    re-org

---
+++ algorithms/src/dsa/arrays/test1.py
@@ -0,0 +1,34 @@
+import collections
+import itertools
+
+ransomNote = "aa"
+magazine = "aba"
+print(collections.Counter(ransomNote) - collections.Counter(magazine))
+print(not collections.Counter(ransomNote) - collections.Counter(magazine))
+
+# print(sum(s for s in ['a', 'b']))
+
+x = ['a', 'b']
+print(map(x.index, x))
+
+nums = [1,2,1,1,2,1,2]
+for k, v in itertools.groupby(nums):
+    for w in v:
+        print(f'{k}: {w}')
+    print(' ')
+# print(list(itertools.groupby(nums)))
+
+s = 'ab12345'
+for prev_ch, ch in itertools.pairwise(itertools.chain('0', s)):
+    print(f'{prev_ch}: {ch}')
+
+print(list(itertools.chain('0', 'abc')))
+
+x = 3
+y = 7
+print(bin(x^y))
+
+print(int("0", 16))
+
+b = "1110"
+print(b.count('0') * b.count('1') )
